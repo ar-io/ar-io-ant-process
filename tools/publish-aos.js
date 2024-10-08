0cc0cd4712fd16063b1b6bb12861f5b1d4aeaafe
atticusofsparta <atticusofsparta@protonmail.com>
2024-10-08 09:19:07 -0600

    chore(publish): add ci cd for publish workflow

--- tools/publish-aos.js
+++ tools/publish-aos.js
@@ -1,6 +1,16 @@
 const Arweave = require('arweave');
 const path = require('path');
 const fs = require('fs');
+const { TurboFactory } = require('@ardrive/turbo-sdk');
+const { createData, ArweaveSigner } = require('@dha-team/arbundles');
+
+const changelog = `
+# Changelog
+
+### Changed
+
+- Changed default landing page transaction ID.
+`;
 
 const srcCodeTxIdPlaceholder = '__INSERT_SOURCE_CODE_ID__';
 
@@ -14,41 +24,61 @@ async function main() {
     path.join(__dirname, '../dist/aos-bundled.lua'),
     'utf-8',
   );
-  const wallet = fs.readFileSync(path.join(__dirname, 'key.json'), 'utf-8');
+  let wallet = process.env.ARWEAVE_PUBLISHING_KEY;
+  if (!wallet) {
+    try {
+      wallet = fs.readFileSync(path.join(__dirname, 'key.json'), 'utf-8');
+    } catch (error) {
+      console.error(
+        'No key found, please set ARWEAVE_PUBLISHING_KEY environment variable or provide a key.json file',
+      );
+      return;
+    }
+  }
   const jwk = JSON.parse(wallet);
   const address = await arweave.wallets.jwkToAddress(jwk);
+  const signer = new ArweaveSigner(jwk);
+  const turbo = TurboFactory.authenticated({ signer });
 
-  const tx1 = await arweave.createTransaction({ data: bundledLua }, jwk);
-  tx1.addTag('App-Name', 'aos-LUA');
-  tx1.addTag('App-Version', '0.0.1');
-  tx1.addTag('Content-Type', 'text/x-lua');
-  tx1.addTag('Author', 'Permanent Data Solutions');
-  await arweave.transactions.sign(tx1, jwk);
-  await arweave.transactions.post(tx1);
+  const publishingTags = Object.entries({
+    'App-Name': 'aos-LUA',
+    'App-Version': '0.0.1',
+    'Content-Type': 'text/x-lua',
+    Author: 'Permanent Data Solutions',
+    Changelog: changelog,
+  }).map(([name, value]) => ({ name, value }));
+
+  const data1 = await createData(bundledLua, signer, {
+    tags: publishingTags,
+  });
+  await data1.sign(signer);
+  const dataTx1 = await turbo.uploadSignedDataItem({
+    dataItemSizeFactory: () => data1.getRaw().byteLength,
+    dataItemStreamFactory: () => data1.getRaw(),
+  });
 
   // replace placeholder with actual tx id
-  const bundledLuaWithTxId = bundledLua.replace(srcCodeTxIdPlaceholder, tx1.id);
-
-  console.log(`Publish AOS ANT Lua with address ${address}`);
-
-  const tx = await arweave.createTransaction({ data: bundledLuaWithTxId }, jwk);
-  tx.addTag('App-Name', 'aos-LUA');
-  tx.addTag('App-Version', '0.0.1');
-  tx.addTag('Content-Type', 'text/x-lua');
-  tx.addTag('Author', 'Permanent Data Solutions');
-  tx.addTag('Original-Tx-Id', tx1.id);
-  tx.addTag(
-    'Changelog',
-    `# Changelog
-
-### Changed
-
-- Changed default landing page transaction ID.
-`,
+  const bundledLuaWithTxId = bundledLua.replace(
+    srcCodeTxIdPlaceholder,
+    dataTx1.id,
   );
-  await arweave.transactions.sign(tx, jwk);
-  await arweave.transactions.post(tx);
 
-  console.log('Transaction ID:', tx.id);
+  const data2 = await createData(bundledLuaWithTxId, signer, {
+    tags: [...publishingTags, { name: 'Original-Tx-Id', value: dataTx1.id }],
+  });
+  await data2.sign(signer);
+
+  const dataTx2 = await turbo.uploadSignedDataItem({
+    dataItemSizeFactory: () => data2.getRaw().byteLength,
+    dataItemStreamFactory: () => data2.getRaw(),
+  });
+
+  console.log(
+    JSON.stringify({
+      ['Lua Code Transaction ID']: dataTx2.id,
+      ['Changelog']: changelog,
+      ['Published with']: address,
+    }),
+  );
 }
 main();

91c4c5bd62065336d70909776d951e35d8ee5cc5
dtfiedler <dylan@ar.io>
2024-10-14 20:36:19 -0500

    chore(test): remove unused regex
    We have to be careful with regex in lua 5.3. It is limited, so everything should be sufficiently tested

--- src/common/constants.lua
+++ src/common/constants.lua
@@ -2,10 +2,6 @@ local constants = {}
 
 constants.MAX_UNDERNAME_LENGTH = 61
 constants.UNDERNAME_DOES_NOT_EXIST_MESSAGE = "Name does not exist in the ANT!"
-constants.UNDERNAME_REGEXP = "^(?:@|[a-zA-Z0-9][a-zA-Z0-9-_]{0,"
-	.. (constants.MAX_UNDERNAME_LENGTH - 2)
-	.. "}[a-zA-Z0-9])$"
-constants.ARWEAVE_ID_REGEXP = "^[a-zA-Z0-9-_]{43}$"
 constants.INVALID_ARWEAVE_ID_MESSAGE = "Invalid Arweave ID"
 constants.MIN_TTL_SECONDS = 900
 constants.MAX_TTL_SECONDS = 3600
--- src/common/utils.lua
+++ src/common/utils.lua
@@ -1,6 +1,6 @@
 -- the majority of this file came from https://github.com/permaweb/aos/blob/main/process/utils.lua
 
-local constants = require(".constants")
+local constants = require("constants")
 local json = require("json")
 local utils = { _version = "0.0.1" }
 

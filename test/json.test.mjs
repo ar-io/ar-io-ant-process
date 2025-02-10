8fdb81eccfbef70644f95b4af018bddc17975648
atticusofsparta <atticusofsparta@protonmail.com>
2025-02-10 15:24:49 -0600

    fix(memory): bump stack memory to permit 30k records

---
+++ test/json.test.mjs
@@ -0,0 +1,49 @@
+import { createAntAosLoader } from './utils.mjs';
+import { describe, it } from 'node:test';
+import {
+  AO_LOADER_HANDLER_ENV,
+  DEFAULT_HANDLE_OPTIONS,
+} from '../tools/constants.mjs';
+
+describe('JSON limits', async () => {
+  const { handle: originalHandle, memory: startMemory } =
+    await createAntAosLoader();
+
+  async function handle(options = {}, mem = startMemory) {
+    return originalHandle(
+      mem,
+      {
+        ...DEFAULT_HANDLE_OPTIONS,
+        ...options,
+      },
+      AO_LOADER_HANDLER_ENV,
+    );
+  }
+
+  it('should not be able to parse a large json', async () => {
+    const records = {};
+    const controllers = [];
+    const balances = {};
+    for (let i = 0; i < 15_000; i++) {
+      records[`${i}-name`] = {
+        transactionId: ''.padEnd(43, '1'),
+        ttlSeconds: 900,
+      };
+      const controller = ''.padEnd(43, i.toString());
+      controllers.push(controller);
+      balances[controller] = 1;
+    }
+    const data = JSON.stringify({
+      records,
+      controllers,
+      balances,
+    });
+    console.log('parsing data size: ' + data.length);
+    const result = await handle({
+      Data: data,
+      Tags: [{ name: 'Content-Type', value: 'application/json' }],
+    });
+
+    console.dir(result, { depth: null });
+  });
+});
